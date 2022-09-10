68d146f03cc7e3da070d820e474321fa9bfa2d6d
Naveen-1591 <112648740+Naveen-1591@users.noreply.github.com>
2022-09-10 13:19:05 +0530

    09-09-2022
    Model exam

---
+++ Model jumbo burger and small burger.py
@@ -0,0 +1,5 @@
+def numofburgers(tomatoslices,cheeseslices):
+
+    return[tomatoslices//2-cheeseslices,cheeseslices-tomatoslices//2]\
+    if tomatoslices%2==0 and 2*cheeseslices<=tomatoslices<=4*cheeseslices
+else[]
